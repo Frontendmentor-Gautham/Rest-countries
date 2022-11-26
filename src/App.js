589c0f83c75215a964d3a8f81404a706164541d6
Gautham Vanjre <vanjregautham@gmail.com>
2022-11-26 13:18:39 +0530

    completed toggling part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import Navbar from "./Components/Navbar";
+import moonDarkTheme from "./images/moon-dark-theme.svg";
+import moonLightTheme from "./images/moon-light.svg";
 
 function App() {
-  return <Navbar />;
+  const [theme, setTheme] = useState("dark");
+
+  const changeTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
+  useEffect(() => {
+    switch (theme) {
+      case "dark":
+        document.body.classList.add("dark");
+        break;
+      case "light":
+        document.body.classList.remove("dark");
+        break;
+      default:
+        break;
+    }
+  }, [theme]);
+
+  return (
+    <>
+      <nav className={`${theme}-navbar navbar`}>
+        <div>
+          <h1 className={`${theme}-text`}>Where in the world?</h1>
+        </div>
+
+        <div className="theme-container">
+          <img
+            onClick={changeTheme}
+            className={`${theme}-dark-image`}
+            src={moonDarkTheme}
+            alt="Dark theme moon"
+          />
+          <img
+            onClick={changeTheme}
+            className={`${theme}-light-image`}
+            src={moonLightTheme}
+            alt="Dark theme moon"
+          />
+          <h4 className={`${theme}-text`}>Dark Mode</h4>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default App;
