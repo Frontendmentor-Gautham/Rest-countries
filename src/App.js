023eb59f6d6e53d1463ddec0dcfed239f31e2031
Gautham Vanjre <vanjregautham@gmail.com>
2022-11-26 14:38:59 +0530

    totally completed with toggling feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import moonDarkTheme from "./images/moon-dark-theme.svg";
 import moonLightTheme from "./images/moon-light.svg";
+// import searchLight from "./images/search-light.png";
+// import searchDark from "./images/search-dark.png";
 
 function App() {
   const [theme, setTheme] = useState("dark");
@@ -29,6 +31,7 @@ function App() {
 
   return (
     <>
+      {/* Header part */}
       <nav className={`${theme}-navbar navbar`}>
         <div>
           <h1 className={`${theme}-text`}>Where in the world?</h1>
@@ -50,6 +53,31 @@ function App() {
           <h4 className={`${theme}-text`}>Dark Mode</h4>
         </div>
       </nav>
+
+      {/* body part-search input and card componenet */}
+      <div className="input-fields">
+        <input
+          className={`${theme}-input-text`}
+          type="text"
+          name="search-input"
+          id="search-input"
+          placeholder="Search for a country"
+        />
+
+        <select
+          className={`${theme}-region-option`}
+          name="region"
+          id="region-search"
+          placeholder="Filter by region"
+        >
+          <option value="Filter by region">Filter by region</option>
+          <option value="Africa">Africa</option>
+          <option value="America">America</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
     </>
   );
 }
